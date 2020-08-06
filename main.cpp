f1b8b324fa0b7ecd6619e82565258b992fdda9d3
kazuki0099 <minakazudayo44@gmail.com>
2020-08-06 23:09:45 +0900

    HoT: Initial revision

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int h = 0, t = 0;
+    std::cout << "Tossing a coin..." << std::endl;
+    for(int i=0; i<3; i++)
+    {
+        std::cout << "Round " << (i+1) << " ";
+        if((double)rand()/RAND_MAX;<0.5)
+        {
+            std::cout << "Heads" << std::endl;
+            h++;
+        }
+        else
+        {
+            std::cout << "Tails" << std::endl;
+            t++;
+        }
+    }
+    std::cout << "Heads: " << h << ", Tails: " << t << std::endl;
+    if(t<h)
+    {
+        std::cout << name << " won!" << std::endl;
+    }
+    else
+    {
+        std::cout << name << " lost." << std::endl;
+    }
+
+    return 0;
+}
